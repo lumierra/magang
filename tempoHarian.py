91569be2123da064eb325974b216886dac086c00
lumierra <tripoernomoaji@gmail.com>
2018-09-17 14:05:49 +0700

    update scraper tempo

---
+++ tempoHarian.py
@@ -0,0 +1,32 @@
+import datetime
+from Database.dbMongo import Database
+from Scraper.tempo_scraper import Scraper_Tempo
+
+
+ST = Scraper_Tempo()
+db = Database()
+now = datetime.datetime.now().date()
+
+list_category_tempo = ['nasional', 'pemilu', 'pilpres', 'dunia', 'bisnis', 'bola', 'sport', 'seleb', 'tekno', 'otomotif']
+list_name_category_tempo = ['news', 'news', 'news', 'news', 'bisnis', 'sports', 'sports', 'entertainment', 'tekno', 'otomotif']
+
+#delete data from mongoDB
+db.delete_dataDaily('scraper', 'test', 'liputan6.com')
+
+#Get Data
+for x,y in zip(list_category_tempo, list_name_category_tempo):
+    data = ST.get_tempoDaily(x, y, now.year, now.month, now.day)
+
+    attr = []
+    for i in range(len(data)):
+        attr.append(data[i])
+
+    db.insert_data('scraper', 'test', attr)
+
+# data = SL.main()
+#
+# attr = []
+# for i in range(len(data)):
+#     attr.append(data[i])
+#
+# db.insert_data('scraper', 'test', attr)
