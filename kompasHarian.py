d15a0741d1ef081cb862390f35b8d84661d41af9
lumierra <tripoernomoaji@gmail.com>
2018-09-18 13:36:42 +0700

    update file untuk ambil data setahun

--- kompasHarian.py
+++ kompasHarian.py
@@ -6,14 +6,14 @@ SK = Scraper_Kompas()
 db = Database()
 now = datetime.datetime.now().date()
 
-list_category_liputan = ['news', 'ekonomi', 'bola', 'entertainment', 'tekno', 'otomotif']
-list_name_category_liputan = ['news', 'bisnis', 'sports', 'entertainment', 'tekno', 'otomotif']
+list_category_kompas = ['news', 'ekonomi', 'bola', 'entertainment', 'tekno', 'otomotif']
+list_name_category_kompas = ['news', 'bisnis', 'sports', 'entertainment', 'tekno', 'otomotif']
 
 #delete data from mongoDB
 db.delete_dataDaily('scraper', 'test', 'kompas.com')
 
 #Get Data
-for x,y in zip(list_category_liputan, list_name_category_liputan):
+for x,y in zip(list_category_kompas, list_name_category_kompas):
     data = SK.get_dataHarian(x, y, now.year, now.month, now.day)
 
     attr = []
